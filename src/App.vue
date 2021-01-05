a73c087f4c77702e4b6472c22bde1c88703dbbe5
sm-sadi <smsadi19@gmail.com>
2021-01-05 20:07:12 +0100

    modal componet works

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>{{title}} </h1>
+  <!-- <input type="text" ref="names" >
+  <button @click="dothis">clickme </button> -->
+  </div>
+  <div v-if="showModal"> 
+    <Modal :header="header" :text="text" theme = 'sale' @close="toggleModal">
+    <template v-slot:links>
+        <a href="">signup</a>
+    </template>
+    </Modal>
+  </div>
+  <button @click="toggleModal">sign up </button>
+  <br>
+<!-- second  -->
+<div v-if="showModal2"> 
+    <Modal  @close="toggleModal2" theme = 'sale'>
+    <template v-slot:links>
+      <h1>20% code</h1>
+        <p> smsadi.com</p>
+    </template>
+    </Modal>
+  </div>
+  <button @click="toggleModal2">20% off</button>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components : {Modal},
+  data(){
+    return{
+      title : "This is a title",
+      header : "signup for discount code",
+      text: "offer will end soon",
+      showModal : false,
+      showModal2 :false
+      
+    }
+
+  },
+  methods: {
+     
+    toggleModal(){
+      this.showModal = !this.showModal
+    },
+    toggleModal2(){ 
+     this.showModal2 = !this.showModal2
+    }
   }
+
 }
 </script>
 
@@ -22,5 +65,22 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1{
+  color :rgb(20, 10, 10) ;
+/* background-color: rgb(221, 60, 60); */
+}
+.active{
+ height: 300px;
+  }
+button{
+  margin-top: 2px;
+  background: #3498db;
+  border-radius: 28px;
+  font-family: Arial;
+  color: #ffffff;
+  font-size: 20px;
+  padding: 10px 20px 10px 20px;
+  text-decoration: none;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="backdrop" @click.self="closemodal">
+        <div class="modal" :class="{ sale : theme === 'sale' } ">
+            <h1>{{header}}</h1>
+            <p>{{text}}</p>
+            <slot name="links">
+
+            </slot>
+            
+         
+        </div>
+    </div>
+
+
+</template>
+<script>
+export default {
+    props : ['header', 'text' , 'theme'],
+    methods : {
+        closemodal(){
+            this.$emit('close')
+        }
+    }
+}
+</script>
+
+<style>
+ .modal{
+     width: 400px;
+     padding:20px;
+     margin: 100px auto;
+     background:white;
+     border-radius: 10px;
+ }
+ .backdrop{
+     top: 0;
+     position: fixed;
+     background-color: rgb(236 232 232 / 58%);
+     width: 100%;
+     height: 100%;
+ }
+ .modal.sale{
+     background: crimson;
+     color: white;
+ }
+ .modal.h1{
+     color:white;
+ }
+ .modal a{
+    text-decoration :none;
+    color: white;
+    background-color: rgb(18, 38, 216);
+    padding: 5px 5px;
+    border-radius: 4px;
+  }
+</style>
